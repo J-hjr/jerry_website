c8a103b2393ce56ce57fce4fb3c6937e6db6fa3a
Junrui Huang <itsnotjerryh@HJRs-MacBook-Pro.local>
2025-07-03 17:18:30 -0400

    Fix Footer.tsx problem: importing package should follow certain order

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,5 +1,3 @@
-import * as React from 'react'
-
 import { FaEnvelopeOpenText } from '@react-icons/all-files/fa/FaEnvelopeOpenText'
 import { FaGithub } from '@react-icons/all-files/fa/FaGithub'
 import { FaInstagram } from '@react-icons/all-files/fa/FaInstagram'
@@ -10,6 +8,7 @@ import { FaYoutube } from '@react-icons/all-files/fa/FaYoutube'
 import { FaZhihu } from '@react-icons/all-files/fa/FaZhihu'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
+import * as React from 'react'
 
 import * as config from '@/lib/config'
 import { useDarkMode } from '@/lib/use-dark-mode'
